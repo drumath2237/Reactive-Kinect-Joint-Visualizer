2f765545ab29c706dced62156385317612fb511d
drumath <2237k.drumath@gmail.com>
2019-10-14 22:35:02 +0900

    :sparkles: KinectJointDataModelを作った(だけ)

--- Assets/Kinect Joint Visualizer v2/Model/KinectJointDataModel.cs
+++ Assets/Kinect Joint Visualizer v2/Model/KinectJointDataModel.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KinectJointDataModel : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using UniRx;
+using UniRx.Triggers;
 
-    // Update is called once per frame
-    void Update()
+using Kinect = Windows.Kinect;
+
+namespace KinectJointVisualizerV2
+{
+    public class KinectJointDataModel
     {
-        
+        public ReactiveDictionary<Kinect.JointType, Quaternion> _joints = new ReactiveDictionary<Kinect.JointType, Quaternion>()
+        {
+            { Kinect.JointType.Head, Quaternion.identity },
+            { Kinect.JointType.Neck, Quaternion.identity },
+            { Kinect.JointType.SpineShoulder, Quaternion.identity },
+            { Kinect.JointType.ShoulderLeft, Quaternion.identity },
+            { Kinect.JointType.ShoulderRight, Quaternion.identity },
+            { Kinect.JointType.ElbowLeft, Quaternion.identity },
+            { Kinect.JointType.ElbowRight, Quaternion.identity },
+            { Kinect.JointType.WristLeft, Quaternion.identity },
+            { Kinect.JointType.WristRight, Quaternion.identity },
+            { Kinect.JointType.SpineMid, Quaternion.identity },
+            { Kinect.JointType.SpineBase, Quaternion.identity },
+            { Kinect.JointType.HipLeft, Quaternion.identity },
+            { Kinect.JointType.HipRight, Quaternion.identity },
+            { Kinect.JointType.KneeLeft, Quaternion.identity },
+            { Kinect.JointType.KneeRight, Quaternion.identity },
+            { Kinect.JointType.AnkleLeft, Quaternion.identity },
+            { Kinect.JointType.AnkleRight, Quaternion.identity },
+            { Kinect.JointType.FootLeft, Quaternion.identity },
+            { Kinect.JointType.FootRight, Quaternion.identity },
+        };
     }
 }
