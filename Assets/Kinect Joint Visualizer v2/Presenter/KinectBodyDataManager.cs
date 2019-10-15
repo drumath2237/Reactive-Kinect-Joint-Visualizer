426c9d99f8a6dbd1dbf6ee3a9fdc09a2fb15c457
drumath <2237k.drumath@gmail.com>
2019-10-15 09:08:10 +0900

    :sparkles: KinectBodyDataManagerを追加

---
+++ Assets/Kinect Joint Visualizer v2/Presenter/KinectBodyDataManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+
+using Kinect = Windows.Kinect;
+
+namespace KinectJointVisualizerV2
+{
+    public class KinectBodyDataManager
+    {
+
+    }
+}
