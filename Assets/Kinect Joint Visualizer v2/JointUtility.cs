4fd2b510d75833cd34544bfd635a0e3bc0289dd7
drumath <2237k.drumath@gmail.com>
2019-10-15 09:18:49 +0900

    :sparkles: jointUtilityを追加

---
+++ Assets/Kinect Joint Visualizer v2/JointUtility.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Kinect = Windows.Kinect;
+
+namespace KinectJointVisualizerV2
+{
+    public class JointUtility
+    {
+
+        public const float JOINT_SCALE = 1.0f;
+        public static Vector3 GetJointPos(Kinect.Body body, Kinect.JointType type)
+        {
+            return new Vector3(
+                body.Joints[type].Position.X,
+                body.Joints[type].Position.Y,
+                body.Joints[type].Position.Z
+            ) * JOINT_SCALE;
+        }
+
+        public static Quaternion GetJointRot(Kinect.Body body, Kinect.JointType type)
+        {
+            return new Quaternion(
+                body.JointOrientations[type].Orientation.X,
+                body.JointOrientations[type].Orientation.Y,
+                body.JointOrientations[type].Orientation.Z,
+                body.JointOrientations[type].Orientation.W
+            );
+        }
+    }
+}
